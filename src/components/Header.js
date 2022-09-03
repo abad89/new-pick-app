a1fa24a778315fc8c8d7e3857020968fc1b69fd0
abad89 <abadalamenti89@gmail.com>
2022-09-03 00:14:40 -0400

    begin working on scan button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,22 @@
 export default function Header() {
+
+  function handleScan(e) {
+    e.preventDefault()
+    console.log("scanned!")
+    let currentBag = 1
+    let bag = document.getElementsByClassName(`bag${currentBag}`)
+    bag[0].classList.remove('not-picked')
+    bag[0].classList.add('picked')
+    currentBag =+ 1
+  }
+
+
   return (
     <div className='header'>
       <a href="home">New Pick App</a>
       <a href="http://antgbad.com">About</a>
       {/* <span> */}
-        <button>Scan!</button>
+        <button onClick={handleScan}>Scan!</button>
       {/* </span> */}
     </div>
   )
