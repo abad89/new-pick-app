e7d000c2e81071bad6cb24b667880982afbbf943
abad89 <abadalamenti89@gmail.com>
2022-09-09 12:12:32 -0400

    adjust infobox text

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -7,7 +7,7 @@ export default function InfoBox() {
         Click the scan button to replicate physically scanning the bags and cart.
         </p>
         <p>This app demonstrates my idea for a redesign of Amazon's delivery station picking app. 
-          The director leads you to scan each bag in order, and to scan the cart in between in bag. The bag-colored border fills in after the bag has been picked. 
+          The director leads you to scan each bag in order, and to scan the cart in between in bag. The bag-colored border becomes solid after the bag has been picked. 
           After all the bags have been picked, the picker is directed to scan the staging location. Refresh for a new picklist.</p>
       
     </div>
